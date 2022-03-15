70e0672cfb7a651c2d9c7e3511672c590811595d
iMelki <ilan.melki@gmail.com>
2022-03-15 20:40:35 +0200

    First try

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@
 import 'module-alias/register';
 
 import { NestFactory } from '@nestjs/core';
+import * as fs from 'fs';
+import * as path from 'path';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
@@ -9,8 +11,27 @@ import AppModule from './routes/app/app.module';
 
 import AllExceptionsFilter from './filters/all-exceptions.filter';
 
+function buildHttpsOptions() {
+  const ssl: Boolean = process.env.SSL === 'true';
+  let httpsOptions;
+  try {
+    if (ssl) {
+      const keyPath = process.env.SSL_KEY_PATH || '';
+      const certPath = process.env.SSL_CERT_PATH || '';
+      httpsOptions = {
+        key: fs.readFileSync(path.join(__dirname, keyPath)),
+        cert: fs.readFileSync(path.join(__dirname, certPath)),
+      };
+    }
+  } catch {
+    return undefined;
+  }
+  return httpsOptions;
+}
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = undefined; // buildHttpsOptions();
+  const app = await NestFactory.create(AppModule, { httpsOptions });
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionsFilter());
@@ -28,7 +49,9 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   await app.listen(port, async () => {
-    console.log(`The server is running on ${port} port: http://localhost:${port}/api`);
+    // const address = `http${httpsOptions ? 's' : ''}://${process.env.SERVER_HOST}/api`;
+    console.log(`The server is listening at: \n${process.env.SERVER_HOST}`);
   });
 }
+
 bootstrap();
